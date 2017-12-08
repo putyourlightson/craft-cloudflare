02ff89868769ff2a25f82838832999109b462ffc
Mo <m@9dev.de>
2017-12-08 08:52:56 +0100

    Settings JS changes

--- src/resources/cloudflare/dist/js/settings.js
+++ src/resources/cloudflare/dist/js/settings.js
@@ -1,67 +1,91 @@
-$zoneSelect      = $("#settings-zone");
-$purgeUrlField   = $("#settings-urls");
-$verifyContainer = $(".cloudflare-verify");
+'use strict';
 
-$("#settings-cf-test").on('click', function(e){
-    e.preventDefault();
+/*
+ global window,
+ Craft
+ */
 
-    if ($("#settings-apiKey").val() !== '' && $("#settings-email").val() !== '') {
+const zoneSelect              = document.getElementById('settings-zone'),
+      verifyContainer         = document.querySelector('.cloudflare-verify'),
+      verifyCredentialsButton = document.getElementById('settings-cf-test'),
+      purgeUrlsButton         = document.getElementById('settings-purge-urls'),
+      purgeUrlsField          = document.getElementById('settings-urls'),
+      apiKeyField             = document.getElementById('settings-apiKey'),
+      emailField              = document.getElementById('settings-email');
 
-        selectedZoneId = $("#settings-zone option:checked").val();
+verifyCredentialsButton.addEventListener('click', event => {
+  event.preventDefault();
 
-        $.ajax({
-            url: window.FETCH_ZONES_ACTION,
-            type: 'GET',
-            data: { "apiKey": $("#settings-apiKey").val(), "email": $("#settings-email").val() },
-            success: function(data){
-                if (data !== null && data.hasOwnProperty('result')) {
-                    // clear existing options
-                    $zoneSelect.find('option').remove();
+  const apiKey = apiKeyField.value,
+        email  = emailField.value;
 
-                    // append zone options from Cloudflare
-                    for (var i = 0; i < data.result.length; i++) {
-                        var row = data.result[i];
-                        $zoneSelect.append('<option value="'+row.id+'">'+row.name+'</option>');
-                    }
+  if (!apiKey || !email) {
+    return alert(window.__CLOUDFLARE_PLUGIN.messages.noCredentialsEntered);
+  }
 
-                    // restore selection
-                    if (selectedZoneId) {
-                        $zoneSelect.val(selectedZoneId);
-                    }
+  const selectedZoneId = zoneSelect.querySelector('option:checked').value;
 
-                    $verifyContainer.removeClass("fail").addClass("success");
-                } else {
-                    alert('Failed.');
-                    $verifyContainer.removeClass("success").addClass("fail");
-                    console.log(data);
-                }
-            },
-            error: function(data) {
-                alert('Failed.');
-                $verifyContainer.removeClass("success").addClass("fail");
-                console.log(data);
-            }
-        });
-    } else {
-        alert('Please enter an API key and email address first.');
+  Craft.postActionRequest(
+    window.__CLOUDFLARE_PLUGIN.actions.fetchZones,
+    {apiKey, email},
+    (response, statusText, request) => {
+
+      // check for errors
+      if (statusText === 'error' || !data || !data.hasOwnProperty('result')) {
+        alert(window.__CLOUDFLARE_PLUGIN.messages.credentialVerificationFailed);
+
+        verifyContainer.classList.remove('success');
+        verifyContainer.classList.add('fail');
+
+        console.error('Credential verification failed:', response);
+
+        return false;
+      }
+
+      // clear existing options
+      Array.from(zoneSelect.querySelectorAll('option'))
+        .forEach(option => option.remove());
+
+      // append zone options from Cloudflare
+      for (let i of response.result) {
+        const row    = response.result[i];
+        const option = document.createElement('option');
+
+        option.value       = row.id;
+        option.textContent = row.name;
+
+        zoneSelect.appendChild(option);
+      }
+
+      // restore selection
+      if (selectedZoneId) {
+        zoneSelect.value = selectedZoneId;
+      }
+
+      verifyContainer.classList.remove('fail');
+      verifyContainer.classList.add('success');
     }
+  );
+
 });
 
-$("#settings-purge-urls").click(function(e){
-    e.preventDefault();
+purgeUrlsButton.addEventListener('click', event => {
+  event.preventDefault();
 
-    $.ajax({
-        url: window.PURGE_URLS_ACTION,
-        type: 'POST',
-        data: { "urls": $purgeUrlField.val() },
-        success: function(data){
-            $purgeUrlField.val('');
-            alert("URL(s) purged.");
-        },
-        error: function(data) {
-            alert('Failed.');
-            console.log(data);
-        }
-    });
+  const urls = purgeUrlsField.value;
+
+  Craft.postActionRequest(
+    window.__CLOUDFLARE_PLUGIN.actions.purgeUrls,
+    {urls},
+    (response, statusText, request) => {
+      if (statusText === 'error') {
+        console.error('Purge URLs failed:', response);
+        return alert(window.__CLOUDFLARE_PLUGIN.messages.purgeUrlsFailed);
+      }
+
+      purgeUrlsField.value = '';
+
+      return alert(window.__CLOUDFLARE_PLUGIN.messages.purgeUrlsSucceeded);
+    }
+  );
 });
-
