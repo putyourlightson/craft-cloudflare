8770bde44e56f91aa83911624225da6ff04902e6
Matt Stein <matt@workingconcept.com>
2018-05-08 16:16:45 -0700

    More spaces.

--- src/services/RulesService.php
+++ src/services/RulesService.php
@@ -16,114 +16,114 @@ use craft\helpers\UrlHelper;
  */
 class RulesService extends Component {
 
-	/**
-	 * Returns all rules for a table.
-	 * @return array
-	 */
-	public function getRulesForTable(): array
-	{
-		$data  = [];
-		$rules = $this->getRules();
+    /**
+     * Returns all rules for a table.
+     * @return array
+     */
+    public function getRulesForTable(): array
+    {
+        $data  = [];
+        $rules = $this->getRules();
 
-		foreach ($rules as $rule)
-		{
-			$data["{$rule->id}"] = [
-				0 => $rule->trigger,
-				1 => implode("\n", json_decode($rule->urlsToClear))
-			];
-		}
+        foreach ($rules as $rule)
+        {
+            $data["{$rule->id}"] = [
+                0 => $rule->trigger,
+                1 => implode("\n", json_decode($rule->urlsToClear))
+            ];
+        }
 
-		return $data;
-	}
+        return $data;
+    }
 
-	/**
-	 * @return mixed
-	 */
-	public function getRules()
-	{
-		return RuleRecord::find()->all();
-	}
+    /**
+     * @return mixed
+     */
+    public function getRules()
+    {
+        return RuleRecord::find()->all();
+    }
 
-	/**
-	 * @return void
-	 */
-	public function saveRules()
-	{
-		RuleRecord::deleteAll();
+    /**
+     * @return void
+     */
+    public function saveRules()
+    {
+        RuleRecord::deleteAll();
 
-		if ($rulesFromPost = Craft::$app->request->getBodyParam('rules'))
-		{
-			foreach ($rulesFromPost as $row)
-			{
-				$trigger     = $row[0];
-				$urlsToClear = $row[1];
+        if ($rulesFromPost = Craft::$app->request->getBodyParam('rules'))
+        {
+            foreach ($rulesFromPost as $row)
+            {
+                $trigger     = $row[0];
+                $urlsToClear = $row[1];
 
-				$ruleRecord = new RuleRecord();
+                $ruleRecord = new RuleRecord();
 
-				$individualUrls = explode("\n", $urlsToClear);
-				$individualUrls = array_map('trim', $individualUrls);
+                $individualUrls = explode("\n", $urlsToClear);
+                $individualUrls = array_map('trim', $individualUrls);
 
-				$ruleRecord->siteId      = Craft::$app->sites->currentSite->id;
-				$ruleRecord->trigger     = trim($trigger);
-				$ruleRecord->urlsToClear = json_encode($individualUrls);
+                $ruleRecord->siteId      = Craft::$app->sites->currentSite->id;
+                $ruleRecord->trigger     = trim($trigger);
+                $ruleRecord->urlsToClear = json_encode($individualUrls);
 
-				$ruleRecord->save(false);
-			}
-		}
-	}
+                $ruleRecord->save(false);
+            }
+        }
+    }
 
-	/**
-	 * @param string $url
-	 *
-	 * @return void
-	 * @throws \yii\base\Exception
-	 */
-	public function purgeCachesForUrl(string $url)
-	{
-		$relatedRules = $this->getRulesForUrl($url);
-		$urlsToPurge  = [];
+    /**
+     * @param string $url
+     *
+     * @return void
+     * @throws \yii\base\Exception
+     */
+    public function purgeCachesForUrl(string $url)
+    {
+        $relatedRules = $this->getRulesForUrl($url);
+        $urlsToPurge  = [];
 
-		foreach ($relatedRules as $rule)
-		{
-			$urlsToPurge = array_merge($urlsToPurge, json_decode($rule->urlsToClear));
-		}
+        foreach ($relatedRules as $rule)
+        {
+            $urlsToPurge = array_merge($urlsToPurge, json_decode($rule->urlsToClear));
+        }
 
-		// max limit for Cloudflare API
-		if (count($urlsToPurge) > 30)
-		{
-			// TODO: say or do something here!
-		}
+        // max limit for Cloudflare API
+        if (count($urlsToPurge) > 30)
+        {
+            // TODO: say or do something here!
+        }
 
-		for ($i = 0; $i < count($urlsToPurge); $i++)
-		{
-			$urlsToPurge[$i] = UrlHelper::siteUrl($urlsToPurge[$i]);
-		}
+        for ($i = 0; $i < count($urlsToPurge); $i++)
+        {
+            $urlsToPurge[$i] = UrlHelper::siteUrl($urlsToPurge[$i]);
+        }
 
-		Cloudflare::$plugin->cloudflare->purgeUrls($urlsToPurge);
-	}
+        Cloudflare::$plugin->cloudflare->purgeUrls($urlsToPurge);
+    }
 
-	/**
-	 * Get any rules that match a supplied URL.
-	 *
-	 * @param  string $url URL from Craft event that needs to be checked
-	 *
-	 * @return array       rules related to URL
-	 */
-	public function getRulesForUrl(string $url): array
-	{
-		$relatedRules = [];
+    /**
+     * Get any rules that match a supplied URL.
+     *
+     * @param  string $url URL from Craft event that needs to be checked
+     *
+     * @return array       rules related to URL
+     */
+    public function getRulesForUrl(string $url): array
+    {
+        $relatedRules = [];
 
-		if ($rules = $this->getRules())
-		{
-			foreach ($rules as $rule)
-			{
-				if (preg_match("`" . $rule->trigger . "`", $url))
-				{
-					$relatedRules[] = $rule;
-				}
-			}
-		}
+        if ($rules = $this->getRules())
+        {
+            foreach ($rules as $rule)
+            {
+                if (preg_match("`" . $rule->trigger . "`", $url))
+                {
+                    $relatedRules[] = $rule;
+                }
+            }
+        }
 
-		return $relatedRules;
-	}
+        return $relatedRules;
+    }
 }
